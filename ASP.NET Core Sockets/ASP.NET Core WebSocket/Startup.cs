e664fea8a2039c27c2c28caef19b39c3b5aafa45
Vp <vasilhspinis@gmail.com>
2024-04-24 21:28:02 +0300

    WebSocket Implementation for Server-Client
    -Implemented WebSockets for Both the Server Side, and Client Side.
    The client can send a message and the server receives it, and displays it.

---
+++ ASP.NET Core Sockets/ASP.NET Core WebSocket/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net.WebSockets;
+using System.Security.AccessControl;
+using System.Net;
+using System.Text;
+namespace ASP.NET_Core_WebSocket
+{
+    public class Startup
+    {
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            var wsOptions = new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(120) };
+            app.UseWebSockets(wsOptions);
+
+            app.Use(async (HttpContext context, RequestDelegate next) =>
+            {
+                if (context.Request.Path == "/send")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await Send(context, webSocket);
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    }
+                }
+            });
+        }
+    
+        private async Task Send(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024*4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), System.Threading.CancellationToken.None);
+            if (result != null)
+            {
+                while (!result.CloseStatus.HasValue)
+                {
+                    string msg = Encoding.UTF8.GetString(new ArraySegment<byte>(buffer, 0, result.Count));
+                    Console.WriteLine($"{msg}");
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"The Message was Delivered on the server on:{DateTime.UtcNow:f}")), result.MessageType, result.EndOfMessage, System.Threading.CancellationToken.None);
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value,result.CloseStatusDescription, System.Threading.CancellationToken.None);
+
+        }
+    }
+}
---
+++ ASP.NET Core Sockets/Ws.Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+
+namespace ASP.NET_Core_WebSocket
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Press enter to continue...");
+            Console.WriteLine();
+            using (ClientWebSocket client = new ClientWebSocket())
+            {
+                Uri serviceUri = new Uri("Ws://localhost:5221/send");
+                var cTs = new CancellationTokenSource();
+                cTs.CancelAfter(TimeSpan.FromSeconds(120));
+                try
+                {
+                    await client.ConnectAsync(serviceUri, cTs.Token);
+
+                    while (client.State == WebSocketState.Open)
+                    {
+                        Console.WriteLine("Please Enter your Message");
+                        string message = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(message))
+                        {
+                        
+                            string messageToSend = $"{userName}: {message}";
+                            ArraySegment<byte> byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(messageToSend));
+                            await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cTs.Token);
+
+                            var responseBuffer = new byte[1024];
+                            var offset = 0;
+                            var packet = 1024;
+                            while (true)
+                            {
+                                ArraySegment<byte> byteReceived = new ArraySegment<byte>(responseBuffer, offset, packet);
+                                WebSocketReceiveResult response = await client.ReceiveAsync(byteReceived, cTs.Token);
+                                var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                                Console.WriteLine(responseMessage);
+                                if (response.EndOfMessage)
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("The message is empty.");
+                        }
+                    }
+                }
+                catch (WebSocketException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
