f02996ec2bf50efaf405337e7482a65971e07f33
Vp <vasilhspinis@gmail.com>
2024-04-25 11:51:34 +0300

    Changed Time Zone and got rid of warnings
    -Changed time zone from utc to local Greek time.
    -Fix Potential null reference in the result in the startup script.

--- ASP.NET Core Sockets/ASP.NET Core WebSocket/Startup.cs
+++ ASP.NET Core Sockets/ASP.NET Core WebSocket/Startup.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Net.WebSockets;
-using System.Security.AccessControl;
+﻿using System.Net.WebSockets;
 using System.Net;
 using System.Text;
 namespace ASP.NET_Core_WebSocket
@@ -41,23 +35,26 @@ namespace ASP.NET_Core_WebSocket
                 }
             });
         }
-    
+
         private async Task Send(HttpContext context, WebSocket webSocket)
         {
-            var buffer = new byte[1024*4];
+            var buffer = new byte[1024 * 4];
             WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), System.Threading.CancellationToken.None);
             if (result != null)
             {
                 while (!result.CloseStatus.HasValue)
                 {
                     string msg = Encoding.UTF8.GetString(new ArraySegment<byte>(buffer, 0, result.Count));
-                    Console.WriteLine($"{msg}");
-                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"The Message was Delivered on the server on:{DateTime.UtcNow:f}")), result.MessageType, result.EndOfMessage, System.Threading.CancellationToken.None);
+                    Console.WriteLine($"{msg}  {DateTime.Now:f}");
+
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"The Message was Delivered on the server on:{DateTime.Now:f}")), result.MessageType, result.EndOfMessage, System.Threading.CancellationToken.None);
                     result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                 }
-            }
-            await webSocket.CloseAsync(result.CloseStatus.Value,result.CloseStatusDescription, System.Threading.CancellationToken.None);
 
+                if (result != null)
+                    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, System.Threading.CancellationToken.None);
+                return;
+            }
         }
     }
 }
--- ASP.NET Core Sockets/ASP.NET Core WebSocket/obj/Debug/net7.0/ASP.NET Core WebSocket.AssemblyInfo.cs
+++ ASP.NET Core Sockets/ASP.NET Core WebSocket/obj/Debug/net7.0/ASP.NET Core WebSocket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET Core WebSocket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d055f77033243b59976e62b21fa37de38f0643b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET Core WebSocket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET Core WebSocket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
